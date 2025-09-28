f34a2e470b15677cf826cd3d893d2b7428b14db8
ThomasRJensen <60095797+ThomasRJensen@users.noreply.github.com>
2025-09-28 18:25:58 +0200

    Update main.cpp
    Added optimization for rotation - make rainbow table instead of reassigning edges

--- main.cpp
+++ main.cpp
@@ -13,7 +13,16 @@ struct Edge
 
 struct Tile
 {
-    enum class EdgeDirection
+    enum class Rotation : int
+    {
+        _0CW = 0,
+        _90CW = 1,
+        _180CW = 2,
+        _270CW = 3,
+        Max = 4
+    };
+
+    enum class EdgeDirection : int
     {
         Top = 0,
         Right = 1,
@@ -22,18 +31,29 @@ struct Tile
         Max = 4 // Number of edge directions
     };
 
+    static const int NoOfRotations = static_cast<const int>(Rotation::Max);
     static const int NoOfEdges = static_cast<const int>(EdgeDirection::Max);
 
     int id;
     Edge edges[NoOfEdges];
+    Rotation rotation = Rotation::_0CW;
 
     void rotate90cw()
     {
-        Edge tmp = edges[static_cast<const int>(EdgeDirection::Left)];
-        edges[static_cast<const int>(EdgeDirection::Left)] = edges[static_cast<const int>(EdgeDirection::Bottom)];
-        edges[static_cast<const int>(EdgeDirection::Bottom)] = edges[static_cast<const int>(EdgeDirection::Right)];
-        edges[static_cast<const int>(EdgeDirection::Right)] = edges[static_cast<const int>(EdgeDirection::Top)];
-        edges[static_cast<const int>(EdgeDirection::Top)] = tmp;
+        rotation = static_cast<Rotation>(((static_cast<int>(rotation) + 1) % NoOfEdges));
+    }
+
+    static constexpr int rotatedIndex[NoOfRotations][NoOfEdges] =
+    {
+        {0,1,2,3}, //Rotation=0
+        {3,0,1,2}, //Rotation=1
+        {2,3,0,1}, //Rotation=2
+        {1,2,3,0} //Rotation=3
+    };
+
+    const Edge& getEdge(EdgeDirection direction) const
+    {
+        return edges[rotatedIndex[static_cast<int>(rotation)][static_cast<int>(direction)]];
     }
 };
 
@@ -99,8 +119,8 @@ bool backtrack(int pos)
             // Check left neighbor
             if (col > 0)
             {
-                Edge leftNeighborRightEdge = placed[row][col - 1].edges[static_cast<const int>(Tile::EdgeDirection::Right)]; // Neighbor's right
-                Edge myLeft = tiles[i].edges[static_cast<const int>(Tile::EdgeDirection::Left)];
+                Edge leftNeighborRightEdge = placed[row][col - 1].getEdge(Tile::EdgeDirection::Right); // Neighbor's right
+                Edge myLeft = tiles[i].getEdge(Tile::EdgeDirection::Left);
                 if (!matchEdges(leftNeighborRightEdge, myLeft))
                 {
                     ok = false;
@@ -109,8 +129,8 @@ bool backtrack(int pos)
             // Check top neighbor
             if (row > 0)
             {
-                Edge topNeighborBottomEdge = placed[row - 1][col].edges[static_cast<const int>(Tile::EdgeDirection::Bottom)]; // Neighbor's bottom
-                Edge myTop = tiles[i].edges[static_cast<const int>(Tile::EdgeDirection::Top)];
+                Edge topNeighborBottomEdge = placed[row - 1][col].getEdge(Tile::EdgeDirection::Bottom); // Neighbor's bottom
+                Edge myTop = tiles[i].getEdge(Tile::EdgeDirection::Top);
                 if (!matchEdges(topNeighborBottomEdge, myTop))
                 {
                     ok = false;
